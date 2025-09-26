aa713a91ecaa3ac7673c9d312cc9d71be1cff9c9
Matthew <matthewaraujo.dev@gmail.com>
2025-09-26 01:34:07 -0300

    deploy: alterando a origin para meu front

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://leggal-front.vercel.app',
     methods: '*',
     allowedHeaders: '*',
     credentials: true,
